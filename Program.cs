426646c406b57cd973f9ab3aea3a8d368f91336f
Nathan Stiles <stiles.nathan@outlook.com>
2025-02-01 14:47:13 -0500

    sorry this is jacked I'll fix it lateer

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 
 using System.Diagnostics;
 using System.Net;
+using System.Reflection;
 using System.Security.Cryptography;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -13,35 +14,53 @@ omfg
 14mb
 dotnet publish -r win-x64 /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true /p:PublishTrimmed=true --output "C:\abc"
 155kb
-dotnet publish -r win-x64 -p:PublishSingleFile=True --self-contained false --output "C:\abc"
+dotnet publish -r win-x64 -p:PublishSingleFile=True --self-contained false --output "C:\temp\wws-wasm-import"
 */
-public class Config {
+public class Config
+{
     public required string ScriptFile { get; set; }
     public required string ApiKey { get; set; }
     public required string ServerUrl { get; set; }
-    
+
 }
 
 
 public static class Program
 {
     const string CONFIG_FILE = "wasm-upload-cli.json";
-    
+
     const string xSERVER_URL = "https://localhost:7086/api/Wasm/Upload/";
     const string SERVER_URL = "http://www.d1ag0n.com/api/Wasm/Upload/";
     const string SCRIPT_FILE = "script.wasm";
-    private static string ReadLine() {
+    private static string ReadLine()
+    {
         return $"{Console.ReadLine()}";
     }
-    private static void WriteLine(string line) {
+    private static void WriteLine(string line)
+    {
         Console.WriteLine(line);
     }
 
-    
+    public static void Install(string destination) {
+        
+            
+            WriteLine(AppContext.BaseDirectory);
+            WriteLine(AppDomain.CurrentDomain.FriendlyName);
+            var source = Path.Combine(AppContext.BaseDirectory, AppDomain.CurrentDomain.FriendlyName + ".exe");
+            WriteLine($"Installing {source} to {destination}");
+            File.Copy(source, destination, true);
+            WriteLine("okay");
+    }
+
     public static async Task Main(string[] args)
     {
+        if (args != null && args.Length == 2 && args[0] == "install")
+        {
+            Install(args[1]);
+            return;
+        }
         var configPath = Path.Combine(Environment.CurrentDirectory, CONFIG_FILE);
-            
+
         if (!File.Exists(configPath))
         {
             WriteLine($"Config file {CONFIG_FILE} not found. Would you like to create it? (y/N)");
@@ -65,29 +84,32 @@ public static class Program
             WriteLine($"Server URL  : {serverUrl}");
             WriteLine("");
 
-            
+
             WriteLine($"Config file will be written to: '{configPath}'");
-            
+
             WriteLine("Does this look correct? (y/N)");
 
             if (ReadLine().ToLowerInvariant() != "y") return;
-            
-            var newConfig = new Config {
+
+            var newConfig = new Config
+            {
                 ApiKey = apiKey,
                 ScriptFile = scriptFile,
                 ServerUrl = serverUrl
             };
-            var options = new JsonSerializerOptions {
+            var options = new JsonSerializerOptions
+            {
                 WriteIndented = true
             };
             File.WriteAllText(configPath, JsonSerializer.Serialize(newConfig, options));
             WriteLine($"Config file written to '{configPath}'.");
             return;
         }
-        
+
         using var client = new HttpClient();
         var config = JsonSerializer.Deserialize<Config>(File.ReadAllText(configPath));
-        if (config == null) {
+        if (config == null)
+        {
             WriteLine("Configfile not loaded.");
             return;
         }
@@ -95,16 +117,18 @@ public static class Program
 
         var bytes = File.ReadAllBytes(config.ScriptFile);
         form.Add(new ByteArrayContent(bytes), "WASM", "script.wasm");
-        
-        var result = await client.PostAsync(config.ServerUrl + config.ApiKey, form);
 
+        var result = await client.PostAsync(config.ServerUrl + config.ApiKey, form);
+        WriteLine($"http-status {result.StatusCode}");
         if (result.StatusCode == HttpStatusCode.OK)
         {
             var localHash = "SHA256-" + Convert.ToHexString(SHA256.HashData(bytes));
             using var stream = result.Content.ReadAsStream();
             using var reader = new StreamReader(stream);
             var remoteHash = reader.ReadToEnd();
-            if (localHash == remoteHash) {
+            WriteLine($"Server said: {remoteHash}");
+            if (localHash == remoteHash)
+            {
                 WriteLine("Hash match. Good upload.");
             }
             else
@@ -112,6 +136,6 @@ public static class Program
                 WriteLine("Hash mismatch. Bad upload.");
             }
         }
-        
+
     }
 }
---
+++ post.ps1
@@ -0,0 +1,2 @@
+dotnet publish -r win-x64 -p:PublishSingleFile=True --self-contained false --output "C:\temp\wasmupload\"
+c:\temp\wasmupload\wasm-upload-cli.exe install C:\Users\stile\dev\wasm\wws-wasm-import\upload.exe
